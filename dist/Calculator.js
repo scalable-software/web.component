cc5d76c22e1f4d0f0f008176ee418b9c6ff03a3b
Development-Scalable-Software <development@scalable.software>
2024-06-02 09:44:58 -0700

    COMPONENT: Init Build

---
+++ dist/Calculator.js
@@ -0,0 +1,9 @@
+/**
+ * @module Calculator
+ * */
+export class Calculator {
+    static Add = (a, b) => a + b;
+    static Subtract = (a, b) => a - b;
+    static Multiply = (a, b) => a * b;
+    static Divide = (a, b) => a / b;
+}
