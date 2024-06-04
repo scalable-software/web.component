6f360da093a511a5a61d89e49734799190aabaea
Development-Scalable-Software <development@scalable.software>
2024-06-04 09:04:15 -0700

    COMPLETE: Remove Calculator

--- dist/Calculator.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @module Calculator
- * */
-export declare class Calculator {
-    static Add: (a: number, b: number) => number;
-    static Subtract: (a: number, b: number) => number;
-    static Multiply: (a: number, b: number) => number;
-    static Divide: (a: number, b: number) => number;
-}
--- dist/Calculator.js
+++
@@ -1,9 +0,0 @@
-/**
- * @module Calculator
- * */
-export class Calculator {
-    static Add = (a, b) => a + b;
-    static Subtract = (a, b) => a - b;
-    static Multiply = (a, b) => a * b;
-    static Divide = (a, b) => a / b;
-}
