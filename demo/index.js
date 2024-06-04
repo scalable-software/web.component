ca5e02db6cca4599fd7c656c488d3ac359023c09
Development-Scalable-Software <development@scalable.software>
2024-06-04 08:58:21 -0700

    DEMO: Update Demo

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,2 @@
-import { Calculator } from "calculator";
-console.log(`Import module: Calculator`);
-console.log(`Use module: 1 + 2 = ${Calculator.Add(1, 2)}`);
+import { Pin } from "pin";
+customElements.define(Pin.Tag, Pin);
